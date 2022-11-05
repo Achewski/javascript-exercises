161d3877fb096ccab94895a01c4896f5411dc0b6
Achewski <achewski@gmail.com>
2022-11-05 21:17:11 +1300

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, finish) {
+	let sum = 0;
+	if (start < 0 || finish < 0 || !Number.isFinite(start) || !Number.isFinite(finish)) {
+		return "ERROR";
+	}
+	if (start < finish){
+		for (i = start; i <= finish; i++) {
+			sum += i;
+		}
+	} else {
+		for (i = finish; i <= start; i++) {
+			sum += i;
+		}
+	}
+	return sum;
 };
 
 // Do not edit below this line
