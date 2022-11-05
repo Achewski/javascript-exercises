8da423ba06f5a8d4cff4d14e30d07901faec9b37
Achewski <achewski@gmail.com>
2022-11-05 20:01:09 +1300

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+	let stringReversed = "";
+	for (i = string.length; i >= 0; i--) {
+		stringReversed += string.slice(i, i + 1);
+	}
+	return stringReversed;
 };
 
 // Do not edit below this line
