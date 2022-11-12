cdb19687c35d618ef97529188cf0e18072c3d83f
Achewski <achewski@gmail.com>
2022-11-12 20:20:10 +1300

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = member => {
+	if (member < 0) {
+		return "OOPS";
+	} else {
+		let n1 = 0;
+		let n2 = 1;
+		for (i = 1; i <= member; i++) {
+			n3 = (n1 + n2);
+			n1 = n2;
+			n2 = n3;
+		}
+		return n1;
+	}
 };
 
 // Do not edit below this line
