12d89fc47a5942f52a1bf0aad459bf580a4ba20a
Achewski <achewski@gmail.com>
2022-11-12 18:11:19 +1300

    Complete 07_calculator

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function(array, ...args) {
-	return array.filter(items => !args.includes(items));
+const removeFromArray = function(...args) {
+	let array = args[0];
+
+	return array.filter((item) => !args.includes(item));
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,41 @@
-const add = function() {
-	
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const sum = a => {
+	let answer = 0;
+	for (let i = 0; i < a.length; i++) {
+		answer += a[i];
+	}
+	return answer;
 };
 
-const subtract = function() {
-	
+const multiply = a => {
+	let answer = 1;
+	let item;
+	for (i = 0; i < a.length; i++) {
+		item = a[i];
+		answer = answer * item;
+	}
+	return answer;
 };
 
-const sum = function() {
-	
-};
+const power = (a, b) => Math.pow(a, b);
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = a => {
+	let f = 1;
+	for (i = 1; i <= a; i++) {
+		f *= i;
+	}
+	return f;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial
 };
