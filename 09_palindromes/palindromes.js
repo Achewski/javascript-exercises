254c211c111e6ba588502098479b0975a2dd167d
Achewski <achewski@gmail.com>
2022-11-12 19:42:32 +1300

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = string => {
+	let stringReversed = "";
+	string = string.toLowerCase().replace(/[\W]/g, "");
+	for (i = string.length-1; i >= 0; i--) {stringReversed += string[i]};
+	return (stringReversed === string);
 };
 
 // Do not edit below this line
