4d30fbe9c8018013e69851da234877daa3fb1cd2
Achewski <achewski@gmail.com>
2022-11-13 00:20:50 +1300

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = (people) => {
+	people.forEach(person => (!("yearOfDeath" in person))?
+		person["yearOfDeath"] = new Date().getFullYear():
+		person["yearOfDeath"] = person["yearOfDeath"]);
+	oldToYoung = people.sort((a, b) => {
+		ageA = (a["yearOfDeath"] - a["yearOfBirth"]);
+		ageB = (b["yearOfDeath"] - b["yearOfBirth"]);
+		return ageB - ageA;
+	});
+	return oldToYoung[0];
 };
 
 // Do not edit below this line
